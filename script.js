47f776404335b34dbcd72fac4490d416b3d81490
thecyberd3n <thebosscyberden@gmail.com>
2024-08-04 16:24:26 +0100

    changed dir of brython module so it works on github pages also changed test script

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let standardcode
 standardcode = 'from browser import window\nwindow.__BRYTHON__.debug = 0\nprint = window.myPrint\njprint = window.jprint\n'
 
 
-let code = 'print("This is my calculator; Enter 2 numbers to multiply them")\nnum1 = int(input("What is num1? :"))\nnum2 = int(input("What is num2? :"))\nprint("Your number is : " + str(num1 * num2))'
+let code = 'print("This is my calculator; Enter 2 numbers to multiply them")\nnum1 = int(input("What is num1? :"))\nnum2 = int(input("What is num2? :"))\nanswer = str(num1 * num2)\nprint("Your number is : " + answer)'
 
 const textconsole = document.getElementById('console')
 window.textconsole
